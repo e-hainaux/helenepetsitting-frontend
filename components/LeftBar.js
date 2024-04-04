134af7f5270fbd7b9b7644671441df4edd3e7637
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-04-04 18:18:58 +0200

    frontend 04-04-2024

--- components/LeftBar.js
+++ components/LeftBar.js
@@ -10,26 +10,73 @@ import parrot from "../public/Images/parrot.png";
 import snake from "../public/Images/snake.png";
 
 function LeftBar() {
+  // Fonction pour gérer le clic sur chaque image
+  const handleImageClick = (animal) => {
+    console.log(`Vous avez cliqué sur l'image de ${animal}.`);
+    // Ajoutez ici la logique pour ouvrir une modale ou effectuer d'autres actions en fonction de l'image cliquée
+  };
+
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.chatschiens}>
-        <Image src={cat} />
-      </div>
-      <div className={styles.rongeurs}>
-        <Image src={mouse} />
-      </div>
-      <div className={styles.bassecour}>
-        <Image src={chicken} />
-      </div>
-      <div className={styles.chevaux}>
-        <Image src={horse} />
-      </div>
-      <div className={styles.oiseaux}>
-        <Image src={parrot} />
-      </div>
-      <div className={styles.nac}>
-        <Image src={snake} />
-      </div>
+      {/* Image de chat avec bouton cliquable */}
+      <button
+        onClick={() => handleImageClick("chat")}
+        className={styles.imageButton}
+      >
+        <div className={styles.chatschiens}>
+          <Image className={styles.objectFit} src={cat} />
+        </div>
+      </button>
+
+      {/* Image de souris avec bouton cliquable */}
+      <button
+        onClick={() => handleImageClick("souris")}
+        className={styles.imageButton}
+      >
+        <div className={styles.rongeurs}>
+          <Image className={styles.objectFit} src={mouse} />
+        </div>
+      </button>
+
+      {/* Image de poulet avec bouton cliquable */}
+      <button
+        onClick={() => handleImageClick("poulet")}
+        className={styles.imageButton}
+      >
+        <div className={styles.bassecour}>
+          <Image className={styles.objectFit} src={chicken} />
+        </div>
+      </button>
+
+      {/* Image de cheval avec bouton cliquable */}
+      <button
+        onClick={() => handleImageClick("cheval")}
+        className={styles.imageButton}
+      >
+        <div className={styles.chevaux}>
+          <Image className={styles.objectFit} src={horse} />
+        </div>
+      </button>
+
+      {/* Image de perroquet avec bouton cliquable */}
+      <button
+        onClick={() => handleImageClick("perroquet")}
+        className={styles.imageButton}
+      >
+        <div className={styles.oiseaux}>
+          <Image className={styles.objectFit} src={parrot} />
+        </div>
+      </button>
+
+      {/* Image de serpent avec bouton cliquable */}
+      <button
+        onClick={() => handleImageClick("serpent")}
+        className={styles.imageButton}
+      >
+        <div className={styles.nac}>
+          <Image className={styles.objectFit} src={snake} />
+        </div>
+      </button>
     </div>
   );
 }
--- components/MainContent.js
+++ components/MainContent.js
@@ -1,19 +1,48 @@
 import React from "react";
 import styles from "../styles/MainContent.module.css";
+import Image from "next/image";
+import UKflag from "../public/Images/UKflag.png";
 
 function MainContent() {
   return (
     <div className={styles.mainContainer}>
-      <h1>BIENVENUE !</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan
-        pulvinar euismod. Duis et finibus ligula. Curabitur quis elit non quam
-        iaculis consectetur. Aliquam ullamcorper augue ut tortor cursus, eu
-        congue purus iaculis. Nullam ut luctus nisl. Pellentesque habitant morbi
-        tristique senectus et netus et malesuada fames ac turpis egestas.
-        Aliquam aliquet pellentesque velit, a ornare metus tincidunt a. Cras
-        finibus turpis non condimentum faucibus.
-      </p>
+      <div className={styles.buttonsContainer}>
+        <button
+          disabled={false}
+          className={styles.button}
+          onClick={() => {
+            console.log("Clic sur contact !");
+          }}
+        >
+          Contact / Devis gratuit
+        </button>
+        {/* <div className={styles.flagBtn}>
+          <Image
+            src={UKflag}
+            className={styles.flagPic}
+            c
+            onClick={() => {
+              console.log("Clic sur drapeau !");
+            }}
+          />
+        </div> */}
+      </div>
+
+      <div className={styles.txtContainer}>
+        <h1>BIENVENUE !</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan
+          pulvinar euismod. Duis et finibus ligula. Curabitur quis elit non quam
+          iaculis consectetur. Aliquam ullamcorper augue ut tortor cursus, eu
+          congue purus iaculis. Nullam ut luctus nisl. Pellentesque habitant
+          morbi tristique senectus et netus et malesuada fames ac turpis
+          egestas. Aliquam aliquet pellentesque velit, a ornare metus tincidunt
+          a. Cras finibus turpis non condimentum faucibus. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Nam accumsan pulvinar euismod. Duis
+          et finibus ligula. Curabitur quis elit non quam iaculis consectetur. -
+          FIN DU PARAGRAPHE -
+        </p>
+      </div>
     </div>
   );
 }
