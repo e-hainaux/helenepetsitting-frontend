5bc68d4fb8993194ad5a75ea8097e6ec7e2f14e3
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-04-09 15:19:15 +0200

    MEP formulaire valide en localhost

--- components/ContactDevis.js
+++ components/ContactDevis.js
@@ -1,21 +1,119 @@
 import React, { useState } from "react";
 import styles from "../styles/ContactDevis.module.css";
-// import ReCAPTCHA from "react-google-recaptcha";
+import ReCAPTCHA from "react-google-recaptcha";
 
 import BackBar from "./BackBar";
 
 function ContactDevis({ onClose }) {
+  const siteKey = "6Lcex7UpAAAAAHgNLCM4__RoLiRNQGP9haO23Iu8";
+
   const [token, setToken] = useState(null);
   const [prenom, setPrenom] = useState("");
   const [nom, setNom] = useState("");
-  const [societe, setSociete] = useState("");
+  // const [societe, setSociete] = useState("");
   const [email, setEmail] = useState("");
   const [telephone, setTelephone] = useState("");
   const [commentaire, setCommentaire] = useState("");
   const [message, setMessage] = useState("");
 
-  const handleSubmit = () => {
-    console.log("Clic sur submit !");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Vérification des champs obligatoires
+    if (!prenom || !nom || (!email && !telephone)) {
+      setMessage(
+        "Les champs prénom, nom et au moins un moyen de contact (email ou téléphone) sont obligatoires."
+      );
+      return;
+    }
+
+    // Validation des champs avec des expressions régulières
+    const prenomRegex = /^[A-Za-zÀ-ÿ-]{1,20}$/;
+    const nomRegex = /^[A-Za-zÀ-ÿ-]{1,20}$/;
+    // const societeRegex = /^[A-Za-z0-9.-]*$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const telephoneRegex = /^(0|\+33|0033)[1-9]([-. ]?[0-9]{2}){4}$/;
+
+    if (!prenomRegex.test(prenom)) {
+      setMessage(
+        "Le prénom doit utiliser des lettres uniquement et contenir au maximum 20 caractères."
+      );
+      return;
+    }
+
+    if (!nomRegex.test(nom)) {
+      setMessage(
+        "Le nom doit utiliser des lettres uniquement et contenir au maximum 20 caractères."
+      );
+      return;
+    }
+
+    // if (societe && !societeRegex.test(societe)) {
+    //   setMessage(
+    //     "La société doit contenir uniquement des lettres, des chiffres et les caractères point et tiret simple."
+    //   );
+    //   return;
+    // }
+
+    if (email && !emailRegex.test(email)) {
+      setMessage(
+        "L'adresse mail doit correspondre au format adresse@site.extension."
+      );
+      return;
+    }
+
+    if (telephone && !telephoneRegex.test(telephone)) {
+      setMessage(
+        "Le numéro de téléphone doit utiliser uniquement des chiffres et correspondre à un numéro de portable ou de fixe français."
+      );
+      return;
+    }
+
+    // Nettoyage du commentaire en supprimant tout le code HTML
+    const cleanedCommentaire = commentaire.replace(/(<([^>]+)>)/gi, "");
+
+    // Données à envoyer au backend
+    const formData = {
+      token,
+      prenom,
+      nom,
+      // societe,
+      email,
+      telephone,
+      commentaire: cleanedCommentaire,
+    };
+
+    try {
+      // Envoi des données au backend
+      const response = await fetch(
+        "https://hps-frontend.vercel.app/form/send-email",
+        {
+          method: "POST",
+          headers: {
+            // Origin: "https://sya-frontend.vercel.app",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.ok) {
+        setMessage("Votre formulaire a été soumis avec succès.");
+        // Réinitialisation du formulaire après soumission
+        setPrenom("");
+        setNom("");
+        // setSociete("");
+        setEmail("");
+        setTelephone("");
+        setCommentaire("");
+      } else {
+        setMessage("Erreur lors de l'envoi du formulaire. Veuillez réessayer.");
+      }
+    } catch (error) {
+      setMessage("Erreur lors de l'envoi du formulaire. Veuillez réessayer.");
+      console.error("Erreur lors de l'envoi du formulaire:", error);
+    }
+    setToken(null);
   };
   return (
     <div className={styles.mainContainer}>
@@ -95,10 +193,10 @@ function ContactDevis({ onClose }) {
             </div>
             <div className={styles.message}>{message}</div>
             <div className={styles.btnContainer}>
-              {/* <ReCAPTCHA
+              <ReCAPTCHA
                 sitekey={siteKey}
                 onChange={(value) => setToken(value)}
-              /> */}
+              />
               <button disabled={!token} type="submit" className={styles.button}>
                 Envoyer
               </button>
